53be5de6d1df97f201f6e6e3497fa1d44f0fbe57
niel <dhrannydan2018@gmail.com>
2022-03-08 14:00:02 +0100

    Working on result models

---
+++ src/main/java/com/skool/models/Result.java
@@ -0,0 +1,36 @@
+package com.skool.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.HashMap;
+
+@Entity
+public class Result {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    private int level;
+
+    private int passScore;
+
+    private int semester;
+
+    private int gpa;
+
+    private int cgpa;
+
+    private HashMap<Long, Integer> scores = new HashMap();
+
+    private long studentId;
+
+    public boolean ifPass(long courseId){
+        int score = scores.get(courseId);
+        if (score >= passScore)
+            return true;
+        return false;
+    }
+}
